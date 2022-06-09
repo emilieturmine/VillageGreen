d5590ab292693b517c0f9e718814d2e416dfcc25
emilieturmine <emilie.turmine@gmail.com>
2022-06-09 10:47:43 +0200

    debut panier  09/06/2022

---
+++ src/Controller/PanierController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+
+#[Route('/panier', name: 'panier_')]
+class PanierController extends AbstractController
+{
+    #[Route('/panier', name: 'app_panier')]
+    public function index(): Response
+    {
+        return $this->render('panier/index.html.twig', [
+            'controller_name' => 'PanierController',
+        ]);
+    }
+
+    #[Route("/add/{id}", name:"add")]
+public function add($id,SessionInterface $session){
+
+
+}
+
+
+
+
+
+
+
+
+
+}
